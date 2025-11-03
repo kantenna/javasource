6f96ae7138a981895374e8842fd53a5e1c843102
김건우 <kantenna11@gmail.com>
2025-11-03 18:01:18 +0900

    JDBC_DTO_DAO

---
+++ ch4_jdbc/src/student2/dto/DeptDTO.java
@@ -0,0 +1,15 @@
+package student2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// @Data == @ToString, @Getter, @Setter, @RequiredArgsConstructor, @EqualsAndHashCode
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class DeptDTO {
+    private String deptId;
+    private String deptName;
+}
---
+++ ch4_jdbc/src/student2/dto/ProfessorDTO.java
@@ -0,0 +1,62 @@
+package student2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+// ~~DTO(Data Transfer Object) == ~~VO(Value Object)
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ProfessorDTO {
+    private String profId;
+    private String profname;
+    private String deptId;
+
+    // 생성자
+    // public ProfessorDTO() {
+    // }
+
+    // public ProfessorDTO(String profId, String profname, String deptId) {
+    // this.profId = profId;
+    // this.profname = profname;
+    // this.deptId = deptId;
+    // }
+
+    // // getter, setter, toString
+    // public String getProfId() {
+    // return profId;
+    // }
+
+    // public void setProfId(String profId) {
+    // this.profId = profId;
+    // }
+
+    // public String getProfname() {
+    // return profname;
+    // }
+
+    // public void setProfname(String profname) {
+    // this.profname = profname;
+    // }
+
+    // public String getDeptId() {
+    // return deptId;
+    // }
+
+    // public void setDeptId(String deptId) {
+    // this.deptId = deptId;
+    // }
+
+    // @Override
+    // public String toString() {
+    // return "ProfessorDTO [profId=" + profId + ", profname=" + profname + ",
+    // deptId=" + deptId + "]";
+    // }
+
+}
